ea2e6b91ba96a518ec2e2ee0a7a535869807fa70
dsellke <dsellke@purdue.edu>
2024-08-28 13:39:15 -0400

    Add project files.

---
+++ lab2CGT215/lab2CGT215.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	float A;
+	float B;
+	float X;
+	cout << "Please enter a value for A: ";
+	cin >> A;
+	cout << "Please enter a value for B: ";
+	cin >> B;
+	cout << "A=" << A << endl;
+	cout << "B=" << B<< endl;
+	A*(X)+B = 0;
+	cout << "And X=" << B << endl;
+}
